3bb6f308b18af3c5957a967b8dad4d8dcd65f8a5
Oliver Schwartz <olsch88@googlemail.com>
2023-12-08 09:10:04 +0100

    setup for day 8

---
+++ d8.py
@@ -0,0 +1,37 @@
+import time
+
+
+def solve_part1(data: list[str]):
+    return 0
+
+
+def solve_part2(data: list[str]):
+    return 0
+
+
+def read_data(input_file: str):
+    with open(input_file, "r") as file:
+        data = file.readlines()
+    data = [line.strip() for line in data]
+    return data
+
+
+def main():
+    day = 8
+
+    data = read_data(f"d{day}_input.txt")
+    # data = read_data(f"d{day}_sample.txt")
+
+    start_time = time.perf_counter_ns()
+    print(f"Solution Day {day}, Part1:")
+    print(solve_part1(data))
+    print(f"Time for part 1: {(time.perf_counter_ns()-start_time)/1000} ms")
+
+    start_time = time.perf_counter_ns()
+    print(f"Solution Day {day}, Part2:")
+    print(solve_part2(data))
+    print(f"Time for part 2: {(time.perf_counter_ns()-start_time)/1000} ms")
+
+
+if __name__ == "__main__":
+    main()
