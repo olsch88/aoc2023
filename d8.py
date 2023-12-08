1b75c1b25b5bb883f50d164257c323c4b623638c
olsch88 <olsch88@googlemail.com>
2023-12-08 21:51:11 +0100

    d8 part2

--- d8.py
+++ d8.py
@@ -1,15 +1,71 @@
 import time
+import itertools
+import functools
+import math
+
+DIRECTION = {"L": 0, "R": 1}
 
 
 def get_nodes(data: list[str]) -> dict[str, tuple[str, str]]:
-    pass
+    nodes = dict()
+    for i, line in enumerate(data):
+        if i < 2:
+            continue
+        key = line.split("=")[0].strip()
+        value1 = line.split("=")[1].strip().strip("()").split(",")[0].strip()
+        value2 = line.split("=")[1].strip().strip("()").split(",")[1].strip()
+        nodes[key] = (value1, value2)
+
+    return nodes
 
 
 def solve_part1(data: list[str]):
+    instructions = data[0].strip()
+    nodes = get_nodes(data)
+
+    path_length = 0
+    curent_location = "AAA"
+    for instr in itertools.cycle(instructions):
+        curent_location = nodes[curent_location][DIRECTION[instr]]
+        path_length += 1
+        if curent_location == "ZZZ":
+            return path_length
     return 0
 
 
 def solve_part2(data: list[str]):
+    instructions = data[0].strip()
+    nodes = get_nodes(data)
+    current_nodes = [node for node in nodes.keys() if node.endswith("A")]
+    start_nodes = current_nodes[:]
+    cycle_times = [0 for a in range(len(current_nodes))]
+    nodes_seen = [[] for a in range(len(current_nodes))]
+    path_length = -1
+    next_nodes = []
+    for j, instr in enumerate(itertools.cycle(instructions)):
+        cur_inst_pos = j % len(instructions)
+        path_length += 1
+        for i, node in enumerate(current_nodes):
+            next_node = nodes[node][DIRECTION[instr]]
+            # if next_node in nodes_seen[i][0] and cycle_times[i] == 0:
+            #     cycle_times[i] = path_length
+            # else:
+            #     nodes_seen[i].append(next_node)
+            if next_node.endswith("Z"):
+                cycle_times[i] = path_length + 1
+            next_nodes.append(next_node)
+            # if next_node == start_nodes[i]:
+            #     cycle_times[i] = path_length
+        if all([node.endswith("Z") for node in next_nodes]):
+            return path_length
+        if all([n > 0 for n in cycle_times]):
+            # print(cycle_times)
+            # print(nodes_seen)
+            print(math.lcm(*cycle_times))
+            return functools.reduce((lambda x, y: x * y), cycle_times)
+
+        current_nodes = next_nodes
+        next_nodes = []
     return 0
 
 
@@ -24,11 +80,12 @@ def main():
     day = 8
 
     data = read_data(f"d{day}_input.txt")
-    # data = read_data(f"d{day}_sample.txt")
+
+    # data = read_data(f"d{day}_sample3.txt")
 
     start_time = time.perf_counter_ns()
     print(f"Solution Day {day}, Part1:")
-    print(solve_part1(data))
+    # print(solve_part1(data))
     print(f"Time for part 1: {(time.perf_counter_ns()-start_time)/1000} ms")
 
     start_time = time.perf_counter_ns()
