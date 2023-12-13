e559a6576f7acab3db6f9862d9673ae42eb16340
olsch88 <olsch88@googlemail.com>
2023-12-13 23:32:39 +0100

    setup for day 13

---
+++ d13.py
@@ -0,0 +1,37 @@
+import time
+
+
+def solve_part1(data: list[str]):
+    return 0
+
+
+def solve_part2(data: list[str]):
+    return 0
+
+
+def read_data(input_file: str):
+    with open(input_file, "r") as file:
+        data = file.readlines()
+    data = [line.strip() for line in data]
+    return data
+
+
+def main():
+    day = 13
+
+    data = read_data(f"d{day}_input.txt")
+    # data = read_data(f"d{day}_sample.txt")
+
+    start_time = time.perf_counter_ns()
+    print(f"Solution Day {day}, Part1:")
+    print(solve_part1(data))
+    print(f"Time for part 1: {(time.perf_counter_ns()-start_time)/1000} µs")
+
+    start_time = time.perf_counter_ns()
+    print(f"Solution Day {day}, Part2:")
+    print(solve_part2(data))
+    print(f"Time for part 2: {(time.perf_counter_ns()-start_time)/1000} µs")
+
+
+if __name__ == "__main__":
+    main()
