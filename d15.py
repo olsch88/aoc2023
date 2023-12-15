0c58fa136bc5fd716d5d5121a9b00cd59127143d
Oliver Schwartz <olsch88@googlemail.com>
2023-12-15 15:02:52 +0100

    d15 part 2

--- d15.py
+++ d15.py
@@ -18,13 +18,54 @@ def solve_part1(data: list[str]) -> int:
 
 
 def solve_part2(data: list[str]):
-    return 0
+    boxes: dict[int, list] = dict()
+    focal_length = 0
+    entry = ""
+
+    for i in range(256):
+        boxes[i] = []
+
+    for instruction in data:
+        # identify the target box
+        if "-" in instruction:
+            label = instruction[:-1]
+        else:
+            label = instruction.split("=")[0]
+
+        box = hash_str(label)
+
+        # fill the boxes
+        if "-" in instruction:
+            for i, element in enumerate(boxes[box]):
+                if element.split()[0] == label:
+                    boxes[box].remove(element)
+                    break
+        elif "=" in instruction:
+            found_in_box = False
+            focal_length = instruction.split("=")[1]
+            entry = label + " " + focal_length
+            for i, element in enumerate(boxes[box]):
+                if element.split()[0] == label:
+                    boxes[box][i] = entry
+                    found_in_box = True
+                    break
+            if not found_in_box:
+                boxes[box].append(entry)
+    total = 0
+    for number, box in boxes.items():
+        for i, element in enumerate(box):
+            total += (number + 1) * (i + 1) * int(element.split()[1])
+
+    return total
 
 
 def read_data(input_file: str):
     with open(input_file, "r") as file:
         data = file.readline()
     return data.split(",")
+
+
+data = read_data(f"d15_input.txt")
 
 
 def main():
@@ -45,5 +86,4 @@ def main():
 
 
 if __name__ == "__main__":
-    print(hash_str("qp"))
     main()
